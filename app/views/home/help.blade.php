f7b0c10922e20334303bbc4143f44efe8017d147
Alan Pachuau <alanpachuau@gmail.com>
2014-02-04 03:01:38 +0530

    Completion stage 4 with video update

--- app/views/home/help.blade.php
+++ app/views/home/help.blade.php
@@ -11,10 +11,10 @@
 		<div class="row">
 			<div class="col-lg-4 col-md-4">
 				<a
-				 href="{{asset('help-videos/author-add.flv')}}"
+				 href="{{asset('help-videos/help.flv')}}"
 				 style="display:block;width:100%;height:250px"  
 				 id="player1"></a>
-				<h5>Author Add</h5>
+				<h5>Help</h5>
 			</div>
 			<div class="col-lg-4 col-md-4">
 				<a
@@ -138,18 +138,59 @@
 			</div>
 			<div class="col-lg-4 col-md-4">
 				<a
-				 href="{{asset('help-videos/settings.flv')}}"
+				 href="{{asset('help-videos/browse-books.flv')}}"
 				 style="display:block;width:100%;height:250px"  
-				 id="player14"></a>
-				 <h5></h5>
+				 id="player17"></a>
+				 <h5>Browse Books</h5>
 			</div>
 			<div class="col-lg-4 col-md-4">
 				<a
-				 href="{{asset('help-videos/user-add.flv')}}"
+				 href="{{asset('help-videos/author-add.flv')}}"
 				 style="display:block;width:100%;height:250px"  
-				 id="player15"></a>
-				 <h5></h5>
+				 id="player18"></a>
+				 <h5>Author Add</h5>
 			</div>
+		</div>
+		
+		<div class="row" style="margin-bottom:50px">
+			<div class="col-lg-4 col-md-4">
+				<a
+				 href="{{asset('help-videos/new-category.flv')}}"
+				 style="display:block;width:100%;height:250px"  
+				 id="player19"></a>
+				<h5>New Category</h5>
+			</div>
+			<div class="col-lg-4 col-md-4">
+				<a
+				 href="{{asset('help-videos/new-issue.flv')}}"
+				 style="display:block;width:100%;height:250px"  
+				 id="player20"></a>
+				 <h5>New Issue</h5>
+			</div>
+			<div class="col-lg-4 col-md-4">
+				<a
+				 href="{{asset('help-videos/issue-return.flv')}}"
+				 style="display:block;width:100%;height:250px"  
+				 id="player21"></a>
+				 <h5>Issue Return</h5>
+			</div>
+		</div>
+		
+		<div class="row" style="margin-bottom:50px">
+			<div class="col-lg-4 col-md-4">
+				<a
+				 href="{{asset('help-videos/issuerecord-cancelreturn.flv')}}"
+				 style="display:block;width:100%;height:250px"  
+				 id="player22"></a>
+				<h5>Issue Record Canceled Return</h5>
+			</div>
+			<div class="col-lg-4 col-md-4">
+				<a
+				 href="{{asset('help-videos/new-publisher.flv')}}"
+				 style="display:block;width:100%;height:250px"  
+				 id="player23"></a>
+				 <h5>New Publisher</h5>
+			</div>	
 		</div>
 		<!-- Video End -->
 	</div>
@@ -157,21 +198,28 @@
 
 <script type="text/javascript">
 $(function(){});
-flowplayer("player1", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player2", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player3", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player4", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player5", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player6", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player7", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player8", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player9", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player10", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player11", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player12", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player13", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player14", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player15", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
-flowplayer("player16", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: false}});
+flowplayer("player1", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player2", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player3", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player4", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player5", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player6", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player7", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player8", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player9", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player10", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player11", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player12", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player13", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player14", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player15", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player16", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player17", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player18", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player19", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player20", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player21", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player22", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
+flowplayer("player23", "{{asset('lib/flowplayer/flowplayer-3.2.18.swf')}}", {clip: {autoPlay: false,autoBuffering: true}});
 </script>
 @stop
